4ab593aa974a85c30cea8258ab05e2b554811d0c
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-01-23 23:11:11 -0500

    Initial Commit

---
+++ podcasts-app/podcasts_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  podcasts_appApp.swift
+//  podcasts-app
+//
+//  Created by Deonte Kilgore on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct podcasts_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
